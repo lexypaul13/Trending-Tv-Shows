829e6781775766c1beed80076ecc5e2284b1cfdc
Alex Paul <lexypaul13@yahoo.com>
2021-01-05 22:19:26 -0500

    Initial Commit

---
+++ Trending Tv Shows/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trending Tv Shows
+//
+//  Created by Alex Paul on 1/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Trending Tv ShowsTests/Trending_Tv_ShowsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Trending_Tv_ShowsTests.swift
+//  Trending Tv ShowsTests
+//
+//  Created by Alex Paul on 1/5/21.
+//
+
+import XCTest
+@testable import Trending_Tv_Shows
+
+class Trending_Tv_ShowsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
